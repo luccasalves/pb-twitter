48adb904bb0f021d0089786f310329edef5c5458
Lucas Alves <as.lucasalves@gmail.com>
2022-10-31 23:32:56 -0300

    feat: compoente de criacao de tweets feito

--- src/components/ScrollFeed/ScroFeed.ts
+++ src/components/ScrollFeed/ScroFeed.ts
@@ -1,6 +1,9 @@
+import { ButtonApp } from "./../ButtonApp/Button";
+import { Avatar } from "./../Avatar/Avatar";
 import { Tweet } from "../Tweet/Tweet";
 import { HeaderApp } from "./../HeaderApp/Header";
 import "./ScrollFeed.css";
+import { IconApp } from "../IconApp/Icon";
 
 export const ScrollFeed = () => {
   const feed = document.createElement("main");
@@ -11,16 +14,17 @@ export const ScrollFeed = () => {
 
   const avatarExemple =
     "https://sm.ign.com/t/ign_br/screenshot/default/34722014736-2d241425f9-k_d66m.h960.jpg";
-  feed.append(
-    HeaderApp({ label: "home" }),
+
+  const tweets = [
     Tweet({
       profileTweet: {
-        name: "Faker",
-        username: "_papaBrocolis",
-        avatarSrc: avatarExemple,
+        name: "Ciclano",
+        username: "_ciclone",
+        avatarSrc: "../../../public/img/2.png",
       },
-      text: "My secret to being the best in the world is eating broccoli",
-      postImg: srcExample,
+      text: "That is a wonderful picture",
+      postImg:
+        "https://images.pexels.com/photos/14036568/pexels-photo-14036568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     }),
     Tweet({
       profileTweet: {
@@ -33,23 +37,48 @@ export const ScrollFeed = () => {
     }),
     Tweet({
       profileTweet: {
-        name: "Faker",
-        username: "_papaBrocolis",
-        avatarSrc: avatarExemple,
+        name: "Frajola",
+        username: "_saurolino",
+        avatarSrc:
+          "https://images.pexels.com/photos/5984809/pexels-photo-5984809.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
       },
-      text: "My secret to being the best in the world is eating broccoli",
-      postImg: srcExample,
+      text: "'w'",
+      postImg:
+        "https://images.pexels.com/photos/674570/pexels-photo-674570.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     }),
-    Tweet({
-      profileTweet: {
-        name: "Faker",
-        username: "_papaBrocolis",
-        avatarSrc: avatarExemple,
-      },
-      text: "My secret to being the best in the world is eating broccoli",
-      postImg: srcExample,
-    })
+  ];
+
+  const createPost = document.createElement("section");
+  const postContent = document.createElement("section");
+  createPost.classList.add("create-post", "surface");
+  postContent.classList.add("post-content", "surface");
+  const textArea = document.createElement("textarea");
+  textArea.classList.add("textarea-post", "surface");
+  textArea.placeholder = "Whats happening?";
+  textArea.rows = 2;
+
+  const actionBar = document.createElement("div");
+  const actionIcons = document.createElement("div");
+  actionBar.classList.add("action-bar", "surface");
+  actionIcons.classList.add("action-icons", "surface");
+
+  actionIcons.append(
+    IconApp({ name: "image", color: "blue", action() {} }),
+    IconApp({ name: "list", color: "blue", action() {} }),
+    IconApp({ name: "face-smile", color: "blue", action() {} }),
+    IconApp({ name: "calendar-day", color: "blue", action() {} }),
+    IconApp({ name: "location-dot", color: "blue", action() {} })
   );
+
+  actionBar.append(
+    actionIcons,
+    ButtonApp({ label: "tweet", size: "sm", action() {} })
+  );
+  postContent.append(textArea, actionBar);
+  createPost.append(Avatar({}), postContent);
+
+  feed.append(HeaderApp({ label: "home" }), createPost);
+  tweets.forEach((t) => feed.append(t));
 
   return feed;
 };
